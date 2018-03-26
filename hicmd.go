15dd47337291d5e7fe0fef3da7e43401745b8f25
yunqiangGao <mrgao.buffoon@gmail.com>
2018-03-26 16:16:27 +0800

    Update hicmd.go

--- hicmd.go
+++ hicmd.go
@@ -1,11 +1,14 @@
-// cfgtest.go
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
+
+const global_version string = "1.0.0"
 
 var (
 	help    bool
@@ -31,6 +34,15 @@ Options:
 -e  edit commond according name
 `
 
+type cmd struct {
+	name        string
+	tag         string
+	file_path   string
+	description string
+}
+
+var cmd_array []cmd
+
 func init() {
 	flag.BoolVar(&help, "h", false, "show help and exit")
 	flag.BoolVar(&version, "v", false, "show version and exit")
@@ -48,7 +60,33 @@ func usage() {
 	fmt.Println(usage_help)
 }
 
+func create_cmd_array() {
+	var cmd_temp cmd
+	var c_name, c_tag, c_description, c_filepath string
+	fi, err := os.Open("/usr/local/hicmd/full_cmd.conf")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	defer fi.Close()
+
+	br := bufio.NewReader(fi)
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		fmt.Sscan(string(a), &c_name, &c_tag, &c_description, &c_filepath)
+		cmd_temp.name = c_name
+		cmd_temp.tag = c_tag
+		cmd_temp.description = c_description
+		cmd_temp.file_path = c_filepath
+		cmd_array = append(cmd_array, cmd_temp)
+	}
+}
+
 func main() {
+	create_cmd_array()
 	flag.Parse()
 
 	if help {
@@ -56,20 +94,43 @@ func main() {
 		os.Exit(0)
 	}
 	if version {
-		fmt.Println("hicmd version: 1.0.0")
+		fmt.Println("hicmd version: ", global_version)
 		os.Exit(0)
 	}
 	if *list {
-		fmt.Println("list all commond")
+		//fmt.Println("list all commond")
+		for _, temp_cmd := range cmd_array {
+			fmt.Println(temp_cmd.name, temp_cmd.tag, temp_cmd.description)
+		}
 		os.Exit(0)
 	}
 
 	if tag != "" {
-		fmt.Println("Tag is " + tag)
+		//fmt.Println("Tag is " + tag)
+		i := 0
+		for _, temp_cmd := range cmd_array {
+			if temp_cmd.tag == tag {
+				fmt.Println(temp_cmd.name, temp_cmd.tag, temp_cmd.description)
+				i++
+			}
+		}
+		if i == 0 {
+			fmt.Println("have no Tag ", tag)
+		}
 		os.Exit(0)
 	}
 	if name != "" {
-		fmt.Println("Name is " + name)
+		//fmt.Println("Name is " + name)
+		i := 0
+		for _, temp_cmd := range cmd_array {
+			if temp_cmd.name == name {
+				fmt.Println(temp_cmd.name, temp_cmd.tag, temp_cmd.description)
+				i++
+			}
+		}
+		if i == 0 {
+			fmt.Println("have no Name ", name)
+		}
 		os.Exit(0)
 	}
 	if edit != "" {
